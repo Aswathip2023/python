9d84cb2a712f397e3b5b0e20311201ea4e3d8fdf
Aswathi P <95677233+Aswathip2023@users.noreply.github.com>
2022-03-02 14:31:21 +0530

    Add files via upload

---
+++ area1 (1) (1) (2).py
@@ -0,0 +1,7 @@
+import rectanglearea
+import sqrarea
+l = int(input("Enter length of rectangle"))
+b = int(input("Enter breadth of rectangle"))
+a = int(input("Enter the length of side of square"))
+rectanglearea.rect(l,b)
+sqrarea.sqr(a)
