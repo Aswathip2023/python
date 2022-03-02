1f78bb377f548395770d2c71d27bb7e2b19024f7
Aswathi P <95677233+Aswathip2023@users.noreply.github.com>
2022-03-02 12:30:08 +0530

    Add files via upload

---
+++ longest word.py
@@ -0,0 +1,12 @@
+def word():
+    text = input("Please input a list of words to evaluate: ")
+
+    longest = 0
+
+    for words in text.split():
+        if len(words) > longest:
+            longest = len(words)
+            longest_word = words
+
+    print("The longest word is", longest_word, "with length", len(longest_word))
+word()
